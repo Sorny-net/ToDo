7b732d16b55fdc42edf83baa2f837a1bd91f8424
SornyNet <sorny.ss@gmail.com>
2020-10-26 11:28:56 +0000

    First Ruby ToDo List

---
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::TodoItemsController < ApplicationController
+  before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @todo_items = TodoItem.all
+    render json: @todo_items
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def set_todo_item
+    @todo_item = TodoItem.find(params[:id])
+  end
+end
